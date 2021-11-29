d309a855559c9ff581ab26c91c5dd78016e15d64
RalphBrynard <ralph@thebrynards.com>
2021-11-29 13:16:48 -0800

    Set working directory

---
+++ terraform/resource_files/README.md
@@ -0,0 +1,6 @@
+# <img src="./assets/Sophos-icon.png" width="35" height="35" />AWS Test Account - Sophos Cloud Optix
+***
+
+
+***
+_BrynardSecurity 2021_
--- terraform/resource_files/vars.tf
+++ terraform/resource_files/vars.tf
@@ -53,3 +53,8 @@ variable "ENABLE_FLOW_ONE_REGION" {
   default = false
   type    = bool
 }
+<<<<<<< Updated upstream
+=======
+
+terraform apply -var "EXTERNAL_ID=e5e21a81-5d17-48af-b404-97137060e586" -var "CUSTOMER_ID=d71a6c02-27f2-4a49-bfed-d91f5fc4e6fe" -var "DNS_PREFIX_FLOW=flow.optix.sophos.com" -var "DNS_PREFIX_CLOUDTRAIL=events.optix.sophos.com" -var "AWS_DEFAULT_REGION=us-east-2" -var "CLOUDTRAIL_S3_RETENTION=90" -var "FLOW_LOGS_S3_RETENTION=1" -var 'FLOWLOG_REGIONS=["us-east-1","us-east-2","us-west-1","us-west-2"]'
+>>>>>>> Stashed changes
