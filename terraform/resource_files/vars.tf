2daed7b95a9bf2ff7950b14097808d00851c0c72
RalphBrynard <ralph@thebrynards.com>
2021-12-01 09:44:36 -0800

    Removed variable from vars.tf

--- terraform/resource_files/vars.tf
+++ terraform/resource_files/vars.tf
@@ -4,9 +4,7 @@ variable "DNS_PREFIX_CLOUDTRAIL" {}
 variable "DNS_PREFIX_FLOW" {}
 variable "FLOW_LOG_S3_BUCKET_PREFIX" {}
 variable "CLOUDTRAIL_S3_BUCKET_PREFIX" {}
-variable "AWS_DEFAULT_REGION" {
-  default = "us-east-2"
-}
+variable "AWS_DEFAULT_REGION" {}
 variable "OPTIX_RESOURCE_KEY" {}
 variable "OPTIX_RESOURCE_VALUE" {}
 variable "FLOW_LOGS_S3_RETENTION" {}
@@ -18,5 +16,4 @@ variable "SET_RETENTION_ON_S3_FLOW" {}
 variable "FLOWLOG_REGIONS" {}
 variable "FLOW_ONE_REGION_VALUE" {}
 variable "FLOW_LOGS" {}
-variable "CLOUDTRAIL_LOGS" {}
 variable "ENABLE_FLOW_ONE_REGION" {}
